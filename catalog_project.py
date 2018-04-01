9fe14124d271f82cd6a93198b4b6b22ccb33735a
vagrant <vagrant@vagrant.vm>
2018-04-01 20:48:23 +0000

    Fixed url paths and added itemDetails.html

--- catalog_project.py
+++ catalog_project.py
@@ -12,20 +12,20 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 #API Endpoint 1
-@app.route('/categories/JSON')
+@app.route('/catalog/JSON')
 def categoryJSON():
     categories = session.query(Category).all()
     return jsonify(Category=[i.serialize for i in categories])
 
 #API Endpoint 2
-@app.route('/category/<int:category_id>/items/JSON')
+@app.route('/catalog/<int:category_id>/items/JSON')
 def itemsJSON(category_id):
     items = session.query(Items).filter_by(category_id = category_id).all()
     return jsonify(Catalog=[i.serialize for i in items])
 
 # Show all categories
 @app.route('/')
-@app.route('/categories/')
+@app.route('/catalog/')
 def showCategories():
     #sqlStatement = text('select i.name, i.creation_date, c.name as category_name from Items i, Category c where i.category_id = c.id order by i.creation_date desc limit 10')
     sqlStatement = 'select i.name, i.creation_date, c.name as category_name from Items i, Category c where i.category_id = c.id order by i.creation_date desc limit 10'
@@ -34,15 +34,21 @@ def showCategories():
     #mostRecent = session.query(Items).order_by(desc(Items.creation_date)).limit(10)
     return render_template('categories.html', categories=categories, mostRecent=mostRecent)
 
-# Show item details
-@app.route('/categories/<int:category_id>/')
-@app.route('/categories/<int:category_id>/item/')
-def showItems(category_id):
-    category = session.query(Category).filter_by(id=category_id).one()
+# Show items for category 
+@app.route('/catalog/<string:category_name>/')
+@app.route('/catalog/<string:category_name>/items/')
+def showItems(category_name):
+    category = session.query(Category).filter_by(name=category_name).one()
     categories = session.query(Category).all()
-    items = session.query(Items).filter_by(category_id=category_id).all()
-    count = session.query(Items).filter_by(category_id=category_id).count()
+    items = session.query(Items).filter_by(category_id=category.id).all()
+    count = session.query(Items).filter_by(category_id=category.id).count()
     return render_template('items.html', items=items, category=category, categories=categories, count = count)
+
+# Show item details
+@app.route('/catalog/<string:category_name>/<string:item_name>/')
+def showItemDetails(category_name, item_name):
+    item = session.query(Items).filter_by(name=item_name).one()
+    return render_template('itemDetails.html', item=item)
 
 if __name__ == '__main__':
     app.debug = True
